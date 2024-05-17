dde353f0650112a698a2be3b2dabf3ae209febfd
Muhammad Huzaifa Waseem <muhammadhuzaifakhan2233@gmail.com>
2024-05-17 22:01:52 +0500

    update

--- core/resources/views/templates/invester/layouts/frontend.blade.php
+++ core/resources/views/templates/invester/layouts/frontend.blade.php
@@ -1,88 +1,255 @@
-@extends($activeTemplate . 'layouts.app')
-@section('panel')
-    <div class="preloader">
-        <div class="animated-preloader"></div>
-    </div>
-    <div class="overlay"></div>
-    <div class="header">
-        <div class="container">
-            <div class="header-bottom">
-                <div class="header-bottom-area align-items-center">
-                    <div class="logo"><a href="{{ route('home') }}"><img src="{{ asset(getImage(getFilePath('logoIcon') . '/logo.png')) }}" alt="logo"></a></div>
-                    <ul class="menu ms-auto">
-                        <li>
-                            <a href="{{ route('home') }}">@lang('Home')</a>
-                        </li>
-                        <li>
-                            <a href="{{ route('plan') }}">@lang('Plan')</a>
-                        </li>
-                        @php
-                            $pages = App\Models\Page::where('tempname', $activeTemplate)
-                                ->where('is_default', 0)
-                                ->get();
-                        @endphp
-                        @foreach ($pages as $k => $data)
-                            <li><a href="{{ route('pages', [$data->slug]) }}">{{ __($data->name) }}</a></li>
-                        @endforeach
-                        <li>
-                            <a href="{{ route('contact') }}">@lang('Contact')</a>
-                        </li>
-                        @if (auth()->check())
-                            <li class="menu-btn">
-                                <a href="{{ route('user.home') }}" class="ps-2"> <i class="las la-user"></i>
-                                    @lang('Dashboard')</a>
-                            </li>
-                        @else
-                            <li>
-                                <a href="{{ route('user.register') }}">@lang('Register')</a>
-                            </li>
-                            <li class="menu-btn">
-                                <a href="{{ route('user.login') }}"> <i class="las la-user"></i> @lang('Login')</a>
-                            </li>
-                        @endif
-                    </ul>
-                    @if ($general->language_switch)
-                        @php
-                            $language = App\Models\Language::all();
-                        @endphp
-                        <select name="langSel" class="langSel form--control h-auto px-2 py-1 border-0">
-                            @foreach ($language as $item)
-                                <option value="{{ $item->code }}" @if (session('lang') == $item->code) selected @endif>
-                                    {{ __($item->name) }}
-                                </option>
-                            @endforeach
-                        </select>
-                    @endif
-                    <div class="header-trigger-wrapper d-flex d-lg-none align-items-center">
-                        <div class="header-trigger">
-                            <span></span>
+<link href="{{ asset('assets/global/css/bootstrap.min.css') }}" rel="stylesheet">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="KeyDesign">
+    <meta name="description" content="AppSperia - App Landing Page">
+    <meta name="keywords" content="AppSperia , Landing page, Template, App, Mobile, Android, iOS">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- SITE TITLE -->
+    <title>AppSperia - App Landing Page</title>
+
+    <!-- FAVICON -->
+    <link rel="icon" href="{{ asset('assets/tix//images/logo.png') }}">
+
+    <!-- WEB FONTS -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:100,300,400,600,700" rel="stylesheet" type="text/css">
+
+    <!-- STYLESHEETS -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/tix/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/tix/fonts/flaticon.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/tix/css/responsive.css') }}">
+    <!-- JQUERY -->
+    <script src="{{ asset('assets/tix/jquery/eWeNyVnhu3iX.1/toXVJeobNhzG.js') }}"></script>
+</head>
+
+<body>
+
+    <!-- PRELOADER -->
+
+    <!-- LANDING PAGE CONTENT -->
+    <div id="fullpage">
+
+        <!-- RIGHT HAND & PHONE MOCK-UP IMAGES -->
+        <div class="wrap">
+            <div class="section-image">
+                <!-- Home IMAGE --><img src="{{ asset('assets/tix/images/1.jpg') }}" alt="">
+                <!-- Features IMAGE --><img src="{{ asset('assets/tix/images/2.jpg') }}" alt="">
+                <!-- About IMAGE --><img src="{{ asset('assets/tix/images/3.jpg') }}" alt="">
+                <!-- Clients IMAGE --><img src="{{ asset('assets/tix/images/4.jpg') }}" alt="">
+                <!-- Download IMAGE --><img src="{{ asset('assets/tix/images/6.jpg') }}" alt="">
+            </div>
+            <div id="hand"></div>
+        </div>
+        <!-- END RIGHT HAND & PHONE MOCK-UP -->
+
+
+        <!-- SECTION HOME -->
+        <div class="section " id="section0">
+            <div class="wrap">
+                <img src="{{ asset('assets/tix/images/logo.png') }}" alt="">
+                <div class="box">
+                    <!-- SECTION HOME CONTENT -->
+                    <h1>
+                        Welcome to Tix: Your <strong>Premier</strong>Trading & Investment Exchange</h1>
+                    <p>
+                        At Tix, we offer a platform where users can invest in our plans with the assurance that their
+                        funds will be managed by our team of professional traders. With years of experience in trading
+                        various assets including cryptocurrencies like Bitcoin on platforms like Binance, our traders
+                        ensure optimal returns on your investment. Here’s how Tix works
+                    </p> <a href="#Download" class="simple-button"><span class="icon flaticon-download7"></span>Download
+                        App</a>
+                </div>
+                <!-- END SECTION HOME CONTENT -->
+            </div>
+        </div>
+        <!-- END SECTION HOME -->
+
+        <!-- SECTION FEATURES -->
+        <div class="section " id="section1">
+            <div class="wrap">
+                <div class="box">
+                    <!-- SECTION FEATURES CONTENT -->
+                    <h2><strong>Our</strong> Mission</h2>
+                    <p>At Tix, our mission is simple yet profound: to empower individuals with the opportunity to
+                        achieve their financial goals through strategic investment and expert trading guidance. We
+                        believe in democratizing access to wealth-building opportunities, regardless of geographical
+                        location or financial background.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!-- END SECTION FEATURES -->
+
+        <!-- SECTION ABOUT -->
+        <div class="section" id="section2">
+            <div class="wrap">
+                <div class="box">
+                    <!-- SECTION ABOUT CONTENT -->
+                    <h2>Our <strong> Values</strong></h2>
+                    <div class="tabs tabs-style-linemove">
+                        <!-- TABS LINKS -->
+                        <nav>
+                            <ul>
+                                <li><a href="#section-linemove-1"><span class="icon flaticon-lightbulb"></span><span>
+                                            Integrity</span></a>
+                                </li>
+                                <li><a href="#section-linemove-2"><span class="icon flaticon-adjust3"></span><span>
+                                            Excellence</span></a>
+                                </li>
+                                <li><a href="#section-linemove-3"><span class="icon flaticon-drawer1"></span><span>
+                                            Innovation</span></a>
+                                </li>
+                                <li><a href="#section-linemove-4"><span class="icon flaticon-laptop3"></span><span>
+                                            Community</span></a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <!-- END TABS LINKS -->
+
+                        <!-- TABS CONTENT -->
+                        <div class="content-wrap">
+
+                            <!-- TAB 1 -->
+                            <section id="section-linemove-1">
+                                <p>We uphold the highest standards of integrity in all our interactions, ensuring
+                                    transparency and trustworthiness at every step.</p>
+                            </section>
+
+                            <!-- TAB 2 -->
+                            <section id="section-linemove-2">
+                                <p>We strive for excellence in everything we do, continuously refining our strategies
+                                    and practices to deliver superior results for our investors.</p>
+                            </section>
+
+                            <!-- TAB 3 -->
+                            <section id="section-linemove-3">
+                                <p>We embrace innovation as a catalyst for progress, leveraging cutting-edge technology
+                                    and insights to stay ahead of the curve in an ever-evolving market landscape.</p>
+                            </section>
+
+                            <!-- TAB 4 -->
+                            <section id="section-linemove-4">
+                                <p>We foster a sense of community among our investors, traders, and team members,
+                                    recognizing that collective collaboration drives collective success.</p>
+                            </section>
                         </div>
+                        <!-- END TABS CONTENT -->
+
+                        <!-- END SECTION ABOUT -->
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+        <!-- END SECTION ABOUT -->
 
-    @yield('content')
+        <!-- SECTION CLIENTS -->
+        <div class="section" id="section4">
+            <div class="wrap">
+                <div class="fp-tableCell clients-content">
+                    <div class="box">
+                        <!-- SECTION CLIENTS CONTENT -->
+                        <h2><strong>Clients</strong> feedbacks</h2>
 
-    @php
-        $content = getContent('footer.content', true);
-    @endphp
-    <!-- Footer Sections -->
-    <footer class="py-4">
-        <div class="container">
-            <div class="footer-content text-center">
-                <a href="{{ route('home') }}" class="logo mb-3"><img src="{{ asset(getImage(getFilePath('logoIcon') . '/logo_2.png')) }}" alt="images"></a>
-                {{-- <p class="footer-text mx-auto">{{ __($content->data_values->content) }}</p> --}}
-                <ul class="footer-links d-flex flex-wrap gap-3 justify-content-center mt-3 mb-3">
-                    <li><a href="{{ route('home') }}" class="link-color">@lang('Home')</a></li>
-                    <li><a href="{{ route('contact') }}" class="link-color">@lang('Contact')</a></li>
-                    <li><a href="{{ route('user.login') }}" class="link-color">@lang('Sign In')</a></li>
-                    <li><a href="{{ route('user.register') }}" class="link-color">@lang('Sign Up')</a></li>
-                </ul>
-                <p class="copy-right-text">&copy; {{ date('Y') }} <a href="{{ route('home') }}" class="text--base">{{ __($general->site_name) }}</a>. @lang('All Rights Reserved')</p>
+                        <!-- CLIENT FEEDBACK 1 -->
+                        <div class="slide" data-anchor="slide1">
+                            <p>"I've been investing with Tix for over a year now, and I couldn't be happier with the
+                                results. The team's professionalism and dedication to maximizing returns while
+                                minimizing risks have truly exceeded my expectations. I highly recommend Tix to anyone
+                                looking for reliable investment opportunities."</p>
+                            <p> <span class="client-name"> <strong> Sarah Ahmed</strong> - Investor</span> <span
+                                    class="client-stars">
+                                    <span class="icon flaticon-little27"></span> <span
+                                        class="icon flaticon-little27"></span> <span
+                                        class="icon flaticon-half3"></span>
+                                    <span class="icon flaticon-fivepointed"></span> <span
+                                        class="icon flaticon-fivepointed"></span> </span>
+                            </p>
+                        </div>
+
+                        <!-- CLIENT FEEDBACK 2 -->
+                        <div class="slide" data-anchor="slide2">
+                            <p>"As a CFO, I understand the importance of prudent investment strategies. Tix has been
+                                instrumental in helping our company manage excess cash reserves effectively. Their
+                                transparent approach and consistent performance have made them our trusted partner in
+                                investment management."</p>
+                            <p> <span class="client-name"> <strong> Michael Johnson</strong> - CFO</span> <span
+                                    class="client-stars">
+                                    <span class="icon flaticon-little27"></span> <span
+                                        class="icon flaticon-little27"></span> <span
+                                        class="icon flaticon-little27"></span> <span
+                                        class="icon flaticon-little27"></span> <span
+                                        class="icon flaticon-fivepointed"></span> </span>
+                            </p>
+                        </div>
+                        <!-- CLIENT FEEDBACK 3 -->
+                        <div class="slide" data-anchor="slide3">
+                            <p>"Navigating the world of investments can be daunting, but Tix made the process seamless
+                                and stress-free for me. From the initial consultation to ongoing support, their team has
+                                been incredibly responsive and knowledgeable. Thanks to Tix, I'm on track to achieve my
+                                financial goals sooner than I ever imagined."</p>
+                            <p> <span class="client-name"> <strong> Emma Patel</strong> - SBO</span> <span
+                                    class="client-stars">
+                                    <span class="icon flaticon-little27"></span> <span
+                                        class="icon flaticon-little27"></span> <span
+                                        class="icon flaticon-little27"></span> <span
+                                        class="icon flaticon-fivepointed"></span> <span
+                                        class="icon flaticon-fivepointed"></span> </span>
+                            </p>
+                        </div>
+                        <!-- END SECTION CLIENTS -->
+                    </div>
+                </div>
             </div>
         </div>
-    </footer>
-    <!-- Footer Section -->
-@endsection
+        <!-- END SECTION CLIENTS -->
+
+        <!-- SECTION DOWNLOAD -->
+        <div class="section" id="section7" data-anchor="Download">
+            <div class="wrap">
+                <div class="box">
+                    <!-- SECTION DOWNLOAD CONTENT-->
+                    <h2><strong>Download</strong> the app</h2>
+                    <p>Whether you're a seasoned investor seeking to diversify your portfolio or a newcomer looking to
+                        take your first steps into the world of trading, Tix is here to support you every step of the
+                        way. Join us today and embark on a journey towards financial freedom and prosperity!
+                    </p>
+                    <!-- DOWNLOAD APPSTORE-->
+                    <a href="#" class="simple-button appstore-button"><span class="icon appstore"></span>App
+                        Store</a>
+                    <!-- DOWNLOAD PLAYSTORE-->
+                    <a href="#" class="simple-button playstore-button"><span class="icon playstore"></span>Play
+                        Store</a>
+                    <!-- END SECTION DOWNLOAD -->
+                </div>
+            </div>
+        </div>
+        <!-- END SECTION DOWNLOAD -->
+
+    </div>
+    <!-- SECTION CONTACT -->
+
+
+    <!-- SOCIAL ICONS -->
+    <div class="wrap">
+        <div id="social-icons">
+            <ul>
+                <li><a href=""><i class="flaticon-facebook6"></i></a> </li>
+                <li><a href=""><i class="flaticon-social19"></i></a> </li>
+                <li><a href=""><i class="flaticon-google16"></i></a> </li>
+                <li><a href=""><i class="flaticon-social40"></i></a> </li>
+                <li><a href=""><i class="flaticon-social7"></i></a> </li>
+                <li><a href=""><i class="flaticon-logo3"></i></a> </li>
+            </ul>
+        </div>
+    </div>
+    <script src="{{ asset('assets/tix/js/jquery.easings.min.js') }}"></script>
+    <script src="{{ asset('assets/tix/js/jquery.fullPage.js') }}"></script>
+    <script src="{{ asset('assets/tix/js/cbpFWTabs.js') }}"></script>
+    <script src="{{ asset('assets/tix/js/jquery.sidr.min.js') }}"></script>
+    <script src="{{ asset('assets/tix/js/scripts.js') }}"></script>
+</body>
+
+</html>
